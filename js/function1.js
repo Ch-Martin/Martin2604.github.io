fc67b0eaa39839ee3b9c197286e648b831ddeb93
Martin Chavez <87248528+Ch-Martin@users.noreply.github.com>
2023-02-01 14:13:17 -0300

    Add files via upload

---
+++ js/function1.js
@@ -0,0 +1,43 @@
+
+document.getElementById('fecha_nacimiento').addEventListener('click', function(){    
+    document.getElementById("demo").innerHTML = "11 de Abril de 1981";
+});
+
+document.getElementById('DNI').addEventListener('click', function(){    
+    document.getElementById("demo").innerHTML = "DNI: 28818607";
+});
+
+document.getElementById('Direccion').addEventListener('click', function(){    
+    document.getElementById("demo").innerHTML = "La Republica 6465, Rosario, Santa Fe";
+});
+
+document.getElementById('mail').addEventListener('click', function(){    
+    document.getElementById("demo").innerHTML = "martinabril1@hotmail.com";
+});
+
+document.getElementById('LinkedIn').addEventListener('click', function(){    
+    document.getElementById("demo").innerHTML = "LinkedInChavezMartin";
+});
+
+
+
+
+
+
+document.getElementById('Secundaria').addEventListener('click', function(){    
+    document.getElementById("demo1").innerHTML = "Secundario";    
+    document.getElementById("demo2").innerHTML = "1999";
+    document.getElementById("demo3").innerHTML = "EET Nº 473";
+});
+
+document.getElementById('HyS').addEventListener('click', function(){
+    document.getElementById("demo1").innerHTML = "Terciario";    
+    document.getElementById("demo2").innerHTML = "2010";
+    document.getElementById("demo3").innerHTML = "Instituto Grotte";
+});
+
+document.getElementById('Robotica').addEventListener('click', function(){    
+    document.getElementById("demo1").innerHTML = "Terciario";    
+    document.getElementById("demo2").innerHTML = "2018";
+    document.getElementById("demo3").innerHTML = "ISET Nº 55";
+});
